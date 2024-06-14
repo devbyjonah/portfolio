9e119c477b6174780b71b434d42b2027377ed87e
jonah wagner <jonahwagner231@gmail.com>
2024-06-14 00:50:20 -0700

    chore: added additional metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,22 @@ export const metadata: Metadata = {
   title: "Jonah Wagner | Software Engineer",
   description:
     "Software engineer with 5+ years of experience building web applications with Javascript/Typescript, React, Node, and NextJS.",
+  applicationName: "Jonah Wagner",
+  metadataBase: new URL("https://jonahwagner.com"),
+  openGraph: {
+    title: "Jonah Wagner | Software Engineer",
+    description:
+      "Software engineer with 5+ years of experience building web applications with Javascript/Typescript, React, Node, and NextJS.",
+    url: "jonahwagner.com",
+    siteName: "Jonah Wagner",
+    images: [
+      {
+        url: "https://www.jonahwagner.com/profile.png",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
